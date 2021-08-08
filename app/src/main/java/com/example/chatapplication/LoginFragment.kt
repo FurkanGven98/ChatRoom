e17f60e3600d8b04cdcded85a8a4b1ba6a982999
FurkanGven98 <53400436+FurkanGven98@users.noreply.github.com>
2021-08-08 15:00:53 +0300

    v1

---
+++ app/src/main/java/com/example/chatapplication/LoginFragment.kt
@@ -0,0 +1,76 @@
+package com.example.chatapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.chatapplication.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
+        if (currentUser != null){
+            val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+            findNavController().navigate(action)
+        }
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signupButton.setOnClickListener {
+        auth.createUserWithEmailAndPassword(binding.emailText.text.toString(),binding.passwordText.text.toString())
+            .addOnSuccessListener {
+                val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+                findNavController().navigate(action)
+            }.addOnFailureListener { exception ->
+            Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
+        binding.loginButton.setOnClickListener {
+            auth.signInWithEmailAndPassword(binding.emailText.text.toString(),binding.passwordText.text.toString())
+                .addOnSuccessListener {
+                    val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+                    findNavController().navigate(action)
+                }.addOnFailureListener { exception ->
+                    Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
+                }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    
+}
